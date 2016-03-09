7f9cc07dd27d9c65ce1ba3177760568f59f0e241
dehowef <dehowef@pc25.cs.ucdavis.edu>
2016-03-09 00:32:36 -0800

    works wooo. need refinements.

--- loader.py
+++ loader.py
@@ -3,6 +3,13 @@ import psycopg2
 import csv
 import sys
 
+def isREAL(txt):
+	try:
+		float(txt)
+		return True
+	except  ValueError:
+		return False
+
 def buildNTHS(filename):
 	
 	if 'PER' in filename:
@@ -20,12 +27,58 @@ def buildNTHS(filename):
 	with (open(filename)) as f:
 		reader = csv.reader(f)
 		row = reader.next()
-		
-		sql = "CREATE TABLE " + tablename + "\n(\n" 
+#create table		
+		sql = "CREATE TABLE IF NOT EXISTS " + tablename + "\n(\n" 
 		for value in row[:-1]:
-			sql += value + ' INT, \n'
-		sql += row[-1] + ' INT\n); '
+			sql += value + ' FLOAT, \n'
+		sql += row[-1] + ' FLOAT\n); '
+		sql = str(sql)
 		print sql
+		cur.execute(sql)
+
+#load table
+	print "Now loading table " + tablename
+	with (open(filename, "rb")) as f:
+		reader = csv.reader(f)
+		row = reader.next()
+		x = 0
+		for row in reader:
+			if(x == 0):
+				sql = "INSERT INTO " + tablename + " VALUES "
+			sql += "(" + row[0] + ', '
+
+			for value in row[1:-1]:
+				if isREAL(value):	
+					sql += value + ', '
+				else:
+					sql += "66666" + ', '
+			
+			if isREAL(row[-1]):
+				sql += row[-1] + ")"
+			else:
+				sql += "66666" + ")"
+			
+			if(x != 1000):
+				sql += ", \n"
+
+			if(x == 1000):
+				sql += "\n ;"
+				sql = str(sql) #need a cast.
+				#print "value of x is ", x
+				#execute here.
+				cur.execute(sql)
+				sql = "" 
+				print "Insertion"
+				x = 0
+				continue
+			x += 1
+
+		if sql != "": 
+			sql = str(sql)[:-3]
+			lastinsert = "INSERT INTO " + tablename + " VALUES " + sql + " ;" 
+			cur.execute(sql)
+			print "final value of x is ", x
+			#insert here.
 
 def buildEIA(filename):
 
@@ -39,36 +92,32 @@ def buildEIA(filename):
 		print "Error, what are you trying to read??"
 		sys.exit()
 
+#create table
 	with (open(filename)) as f:
 		reader = csv.reader(f)
 		row = reader.next()
 		
-		sql = "CREATE TABLE " + tablename + "\n(\n" 
+		sql = "CREATE TABLE IF NOT EXISTS" + tablename + "\n(\n" 
 		for value in row[:-1]:
 			sql += value + ' VARCHAR(100), \n'
 		sql += row[-1] + ' VARCHAR(100)\n); '
-		print sql
+		#print sql
+
+#load table
 
 #open connection
 try:
 	conn = psycopg2.connect(database="postgres", host="/home/" + os.environ['USER'] + "/postgres")
+	cur = conn.cursor()
+	
 	print "Connection Success"
 except:
 	print "Connection Unsuccessful"
 
-"""with (open('PERV2PUB.CSV')) as f:
-	reader = csv.reader(f)
-	row = reader.next()
-	
-	sql = "CREATE TABLE PERV2 \n(\n" 
-	for value in row[:-1]:
-		sql += value + ' INT, \n'
-	sql += row[-1] + ' INT\n); '
-"""
 
 buildNTHS("PERV2PUB.CSV")
-buildNTHS("VEHV2PUB.CSV")
-buildEIA("EIA_CO2_Electricity_2015.csv")
-buildEIA("ERROR.CSV")
-
+#buildNTHS("VEHV2PUB.CSV")
+#buildEIA("EIA_CO2_Electricity_2015.csv")
+# buildEIA("ERROR.CSV")
+cur.close()
 conn.close()
